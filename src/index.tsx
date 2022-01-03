910960f65a4a5e51a1bcd130909686a100e7156f
Andrianarisoa Daniel <radandevist@gmail.com>
2022-01-03 14:27:23 +0300

    ✨ solve echo problem

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ColorModeScript } from "@chakra-ui/react";
 
 import App from "./App";
 // import reportWebVitals from "./utils/reportWebVitals";
@@ -8,7 +7,6 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ColorModeScript />
     <App />
   </React.StrictMode>,
   document.getElementById("root"),
