4d9aa0d11034e963c906db30d709b070bfae5a4a
Andrianarisoa Daniel <radandevist@gmail.com>
2022-01-04 07:18:20 +0300

    🎨 move "continue" btn up

--- src/components/Home/Forms.tsx
+++ src/components/Home/Forms.tsx
@@ -39,6 +39,10 @@ export const FormBase = ({
           </div>
         </div>
 
+        <div>
+          <button onClick={buttonAction} type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">{buttonText}</button>
+        </div>
+
         {(isUserForm)
           ? (
             <div>
@@ -47,10 +51,6 @@ export const FormBase = ({
               </button>
             </div>
           ) : null}
-
-        <div>
-          <button onClick={buttonAction} type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">{buttonText}</button>
-        </div>
       </form>
     </div>
   </div>
